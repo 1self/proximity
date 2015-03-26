9cf5944ef117ac954d7e084c7e9b62f6127e5424
Ed Sykes <ed.sykes@gmail.com>
2015-03-26 22:21:39 +0000

    Added winston logging

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var redis = require('redis');
 var processor = require('./processor');
 var MongoClient = require('mongodb').MongoClient;
+var winston = require('winston');
+
+winston.add(winston.transports.File, { filename: 'proximity.log', level: 'debug', json: false });
+
+winston.error("Errors will be logged here");
+winston.warn("Warns will be logged here");
+winston.info("Info will be logged here");
+winston.debug("Debug will be logged here");
 
 var eventSubscription = redis.createClient();
 eventSubscription.subscribe('events');
