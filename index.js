5e3ee691f518de5ce82ed26d97244fa5c1f7f5b2
Ed Sykes <ed.sykes@gmail.com>
2015-04-16 18:38:45 +0100

    pick up the dburi from env var

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ eventRepository.add = function(event){
 };
 
 // Connection URL
-var url = 'mongodb://localhost:27017/quantifieddev';
+var url = process.env.DBURI;
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
 
